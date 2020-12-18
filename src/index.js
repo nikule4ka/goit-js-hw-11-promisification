32cebe9004074ae4e73f8cf443ced6eedba62758
Nika <yavtushenkonika@gmail.com>
2020-12-18 21:59:56 +0200

    adds tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './styles.css';
+import './task-1';
+import './task-2';
+import './task-3'
+
+// const promise = new Promise((resolve, reject) => {
+//     const canFullField = Math.random() > 0.5;
+
+//     setTimeout(() => {
+//         if (canFullField) {
+//         resolve('all is right, выполнен fullfield');
+//     }
+
+//     reject('промис выполнен с ошибкой, отклонен - rejected');
+//    }, 1000)
+// });
+
+// promise.then(result => {
+//     console.log(`🧩 ${result}`);
+// }, error => {
+//         console.log(`🎯 ${error}`);
+// });
+
+// promise.then(result => {
+//     console.log(result);
+//     return 5;
+// }).then(x => {
+// console.log(x);
+// })
+
+
+
+
+
+
